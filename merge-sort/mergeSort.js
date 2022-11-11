123c95f7d6827f73402cdc08b9e5a9e048b04de0
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-11 10:32:39 +0100

    Added more comments to clarify the steps taken in the algo

--- merge-sort/mergeSort.js
+++ merge-sort/mergeSort.js
@@ -16,19 +16,24 @@ const mergeSort = (arr) => {
 }
 
 const mergeArrays = (leftArr, rightArr) => {
+  // Setting an empty array to push in sorted numbers
   let sortedArray = []
-
+  // Checking that both leftArr and rightArr are not 'undefined'
   while (leftArr.length && rightArr.length) {
+    // Comparing the first element of the leftArr and rightArr for which element is less than the other.
     if (leftArr[0] < rightArr[0]) {
+      // If the first ele of leftArr is less than the first ele of the rightArr, push out first ele of leftArr to sorted array
       sortedArray.push(leftArr.shift())
     } else {
+      // Else, first ele of rightArr must be less than first ele of leftArr, push out first ele of rightArr to sorted array
       sortedArray.push(rightArr.shift())
     }
   }
-
+  // Merge the remainder of both leftArr and rightArr
   return sortedArray.concat([...leftArr]).concat([...rightArr])
 }
 
+// Console logs showing the result of a list of a unsorted array going through merge sort algo
 console.log(mergeSort([9,7]))
 console.log(mergeSort([7,3,6,4,5,1]))
 console.log(mergeSort([9,6,23,20,144,2,10]))
